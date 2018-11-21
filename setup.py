e119efc823ca1aceec401d53b4da1ec41c2278d2
Khoi <hakhoipham@gmail>
2018-11-21 15:28:10 -0500

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 ########################################################################################################################
-### Do not forget to adjust the following variables to your own plugin.
+# Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
 plugin_identifier = "palette2"
@@ -44,22 +44,22 @@ plugin_additional_data = []
 from setuptools import setup
 
 try:
-	import octoprint_setuptools
+    import octoprint_setuptools
 except:
-	print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-	      "the same python installation that OctoPrint is installed under?")
-	import sys
-	sys.exit(-1)
+    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+          "the same python installation that OctoPrint is installed under?")
+    import sys
+    sys.exit(-1)
 
 setup(**octoprint_setuptools.create_plugin_setup_parameters(
-	identifier=plugin_identifier,
-	name=plugin_name,
-	version=plugin_version,
-	description=plugin_description,
-	author=plugin_author,
-	mail=plugin_author_email,
-	url=plugin_url,
-	license=plugin_license,
-	requires=plugin_requires,
-	additional_data=plugin_additional_data
+    identifier=plugin_identifier,
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    mail=plugin_author_email,
+    url=plugin_url,
+    license=plugin_license,
+    requires=plugin_requires,
+    additional_data=plugin_additional_data
 ))
